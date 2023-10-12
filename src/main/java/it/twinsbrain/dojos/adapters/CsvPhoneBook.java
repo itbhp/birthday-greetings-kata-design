0310a4149f15174abc51f5401fa9817dca8ab3ee
Paolo Sciarra <itbhps@gmail.com>
2023-10-12 18:11:09 +0200

    now we parse csv phone book

--- src/main/java/it/twinsbrain/dojos/adapters/CsvPhoneBook.java
+++ src/main/java/it/twinsbrain/dojos/adapters/CsvPhoneBook.java
@@ -3,6 +3,8 @@ package it.twinsbrain.dojos.adapters;
 import it.twinsbrain.dojos.domain.Friend;
 import it.twinsbrain.dojos.domain.PhoneBookReadException;
 import it.twinsbrain.dojos.ports.PhoneBook;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,16 +30,28 @@ public class CsvPhoneBook implements PhoneBook {
       return List.of();
     }
     return Arrays.stream(content.split("\n"))
-            .skip(1)
-            .map(this::toFriend)
-            .collect(Collectors.toList());
+        .skip(1)
+        .map(this::toFriend)
+        .collect(Collectors.toList());
   }
 
-    private Friend toFriend(String csvLine) {
-        String[] columns = csvLine.split(", ");
-        if(columns.length < 4){
-            throw new PhoneBookReadException();
-        }
-        return null;
+  private Friend toFriend(String csvLine) {
+    String[] columns = csvLine.split(", ");
+    if (columns.length < 4) {
+      throw new PhoneBookReadException();
     }
+    var name = columns[1];
+    var lastName = columns[0];
+    var birthDate = parseDate(columns[2]);
+    var email = columns[3];
+    return new Friend(name, lastName, birthDate, email);
+  }
+
+  private LocalDate parseDate(String dateString) {
+    try {
+      return LocalDate.parse(dateString);
+    } catch (DateTimeParseException e) {
+      throw new PhoneBookReadException();
+    }
+  }
 }
--- src/test/java/it/twinsbrain/dojos/adapters/CsvPhoneBookIT.java
+++ src/test/java/it/twinsbrain/dojos/adapters/CsvPhoneBookIT.java
@@ -2,9 +2,10 @@ package it.twinsbrain.dojos.adapters;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
+import it.twinsbrain.dojos.domain.Friend;
 import it.twinsbrain.dojos.domain.PhoneBookReadException;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 class CsvPhoneBookIT {
@@ -45,5 +46,51 @@ class CsvPhoneBookIT {
     var underTest = new CsvPhoneBook(content);
     assertThatThrownBy(underTest::all)
             .isInstanceOf(PhoneBookReadException.class);
+  }
+
+  @Test
+  void malformed_date_throws_error() {
+    // given
+    var content = """
+    last_name, first_name, date_of_birth, email
+    Justin, Bieber, 2000-01, bieber@email.com
+    """.trim();
+
+    // when, then
+    var underTest = new CsvPhoneBook(content);
+    assertThatThrownBy(underTest::all)
+            .isInstanceOf(PhoneBookReadException.class);
+  }
+
+  @Test
+  void one_friend_phone_book() {
+    // given
+    var content = """
+    last_name, first_name, date_of_birth, email
+    Bieber, Justin, 2000-01-01, bieber@email.com
+    """.trim();
+
+    // when, then
+    var underTest = new CsvPhoneBook(content);
+    var bieber = new Friend("Justin", "Bieber", LocalDate.parse("2000-01-01"), "bieber@email.com");
+    assertThat(underTest.all()).containsExactlyInAnyOrder(bieber);
+  }
+
+  @Test
+  void many_friends_phone_book() {
+    // given
+    var content = """
+    last_name, first_name, date_of_birth, email
+    Bieber, Justin, 2000-01-01, bieber@email.com
+    Bieber, Lea, 2004-01-01, lea.bieber@email.com
+    Bieber, Fred, 2010-01-01, fred.bieber@email.com
+    """.trim();
+
+    // when, then
+    var underTest = new CsvPhoneBook(content);
+    var bieber = new Friend("Justin", "Bieber", LocalDate.parse("2000-01-01"), "bieber@email.com");
+    var lea = new Friend("Lea", "Bieber", LocalDate.parse("2004-01-01"), "lea.bieber@email.com");
+    var fred = new Friend("Fred", "Bieber", LocalDate.parse("2010-01-01"), "fred.bieber@email.com");
+    assertThat(underTest.all()).containsExactlyInAnyOrder(bieber, lea, fred);
   }
 }
